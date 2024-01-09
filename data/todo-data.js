a7c0c75de8666b665f1148c43daf2af03848fc6d
richardzk17 <richardhernandezk17@gmail.com>
2024-01-09 15:57:07 -0300

    Games list added

---
+++ data/todo-data.js
@@ -0,0 +1,12 @@
+const games = [
+    {text: 'SpiderMan', Ps5: true, _id: 125223},
+    {text: 'Forza Motorsport', Ps5: false, _id: 127904},
+    {text: 'Halo', ps5: false, _id: 139608},
+    {text: 'Cod', Ps5: true, _id: 125234},
+    {text: 'Gears', Ps5: false, _id: 122304},
+    {text: 'Sea of Thieves', ps5: false, _id: 134308},
+  ]
+  
+  export { 
+      games
+  }
---
+++ server.js
@@ -0,0 +1,36 @@
+// import modules
+
+import express from 'express'
+import { games } from './data/todo-data.js'
+
+// create Express app
+
+const app = express()
+
+// configure the app (app.set)
+
+app.set('view engine', 'ejs')
+
+// mount Middleware (app.use)
+
+
+
+// mount routes
+app.get('/home', function (req, res) {
+  res.render('home')
+})
+
+app.get('/todos', function (req, res) {
+  res.render('todos/index', {
+    games: games
+  })
+})
+
+app.get('/', function (req, res) {
+  res.redirect('/home')
+})
+// tell the app to listen on port 3000
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000')
+})
